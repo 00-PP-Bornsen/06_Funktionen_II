3da53b3bf142c09fa9c8e16645c147b96fb36abb
Peter <schmidt.multimedia@gmail.com>
2023-07-18 11:17:11 +0200

    05a / module: division , 2. Iteration

--- script_01.js
+++ script_01.js
@@ -11,6 +11,8 @@
 4. Ausgabe in Konsole : check!
 */
 
+const ERROR_STR_DIV = "Division durch 0 nicht möglich"
+
 // module: division a / b |  test:
 output(divide(4,2));
 output(divide(3,2));
@@ -18,11 +20,13 @@ output(divide(3,-2));
 output(divide(0,2));
 output(divide(3,0));
 function divide(a,b) {
-	if (b != 0) {
-		return a/b;
-	} else {
-		return "Division durch 0 nicht möglich";
+
+	if (b == 0) { // Ausnahme + Abbruch
+		return ERROR_STR_DIV;
 	}
+	
+	return a/b; // Regel
+
 }
 
 
