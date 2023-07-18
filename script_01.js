cd7fb02899f4412eaa8f5d6198edaec69267c26b
Peter <schmidt.multimedia@gmail.com>
2023-07-18 10:21:05 +0200

    03 /  module: subtraction a - b

--- script_01.js
+++ script_01.js
@@ -12,19 +12,23 @@
 */
 
 
-
-
-
+// module: subtraction a - b |  test:
+output(subtract(3,2));
+output(subtract(3,-2));
+output(subtract(3,0));
+output(subtract(0,3));
+function subtract(a,b) {
+	return a - b;
+}
 
 
 // module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
 function add(a,b) {
 	return a + b;
 }
-
 
 // module: output | test:
 // output("hello");
